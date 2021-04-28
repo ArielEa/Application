1e4aa2e0b2e5ffbce694fc511b81de4095ba05aa
Ariel <ariel@Ariels-MacBook-Pro.local>
2021-04-28 18:21:07 +0800

    redis

--- src/main/java/com/application/javaapplication/conttroller/DemoController.java
+++ src/main/java/com/application/javaapplication/conttroller/DemoController.java
@@ -1,5 +1,23 @@
 package com.application.javaapplication.conttroller;
 
+import com.application.javaapplication.redis.RedisConfig;
+import com.application.javaapplication.redis.RedisUtil;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisClusterConnection;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConnection;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,27 +28,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.Duration;
+import com.application.javaapplication.redis.RedisConfig;
 
 @Controller
 public class DemoController
 {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
     @GetMapping("/")
     @ResponseBody
-    public String index(HttpServletRequest request)
-    {
+    public String index(HttpServletRequest request) {
         String inputKey = request.getParameter("abd");
 
         return "Index Page : out put : " + inputKey;
-
-
     }
 
     @ResponseBody
     @RequestMapping("/post_test")
-    public String postTest(HttpServletRequest request)
-    {
+    public String postTest(HttpServletRequest request) {
         String postKey = request.getParameter("post");
 
-        return "Post key : " + postKey;
+        return "Post key 123 : " + postKey;
+    }
+
+    @RequestMapping("/redis_test")
+    @ResponseBody
+    public String redisStr(HttpServletRequest request)
+    {
+        String message = "";
+
+        message = redisUtil.get("bu");
+
+        return "redis Message : " + message;
     }
 }
---
+++ src/main/java/com/application/javaapplication/redis/RedisAutoConfiguration.java
@@ -0,0 +1,42 @@
+package com.application.javaapplication.redis;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnection;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+import java.net.UnknownHostException;
+
+public class RedisAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public StringRedisTemplate stringRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+}
